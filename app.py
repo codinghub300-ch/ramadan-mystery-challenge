4d8a1769b369090311ddeb18a168f63ce9403aa0
Coding Hub <codinghub300@gmail.com>
2026-06-18 17:30:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,11 @@ def submit():
 
         return jsonify({"success": False})
 
+# if __name__ == "__main__":
+#     import os
+#     port = int(os.environ.get("PORT", 8080))
+#     app.run(host="0.0.0.0", port=port)
+
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
+    
